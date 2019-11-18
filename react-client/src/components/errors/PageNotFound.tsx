9f760cbeaf991b187283823ab6608595ce07922b
Ryan Christian <ryanchristian4427@gmail.com>
2019-11-18 15:17:35 +0000

    Added base style

---
+++ react-client/src/components/errors/PageNotFound.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+import './Errors.scss';
+
+const App: React.FC = () => {
+    return (
+        <div className="error-page">
+            <section className="hero is-coral-light is-bold">
+                <div className="hero-body">
+                    <div className="container">
+                        <h1 className="title">
+                            Sorry
+                        </h1>
+                        <h2 className="subtitle">
+                            This page does not yet exist
+                        </h2>
+                    </div>
+                </div>
+            </section>
+            <section className="card">
+                <div className="level" id="layered-background">
+                    <Link className="button is-spanish-grey level-item" to="/">Back to Safety</Link>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default App;
--- react-client/src/index.tsx
+++ react-client/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'styles/index.css';
 import App from 'components/App';
+
+import 'styles/index.scss';
 
 ReactDOM.render(
     <App />,
