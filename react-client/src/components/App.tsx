20a0e2c650e598480ebbeb4f7aff7ac7f19659f5
Ryan Christian <ryanchristian4427@gmail.com>
2019-11-20 22:55:31 +0000

    feat(routing): Added route for new home page

--- react-client/src/components/App.tsx
+++ react-client/src/components/App.tsx
@@ -5,12 +5,14 @@ import {
   Route,
 } from 'react-router-dom';
 import PageNotFound from 'components/errors/PageNotFound';
+import Home from 'components/Home';
 
 const App: React.FC = () => {
   return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
+              <Home/>
           </Route>
           <Route path="/*">
             <PageNotFound />
