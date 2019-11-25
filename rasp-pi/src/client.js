4f1a63a43c5256c35dee8bb1516987b3cca4eacc
Lauren Heymer <laurenheymer@gmail.com>
2019-11-25 17:56:43 +0000

    ~ Updated api path for Raspberry pi client

--- rasp-pi/src/client.js
+++ rasp-pi/src/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const serverIp = '192.168.1.42';
-const url = `http://${serverIp}:5000/sensorReadings/new`;
+const url = `http://${serverIp}:8000/api/v1/sensorReadings/new`;
 
 export const postNewReading = (sensorReading) => {
     axios.post(url, { sensorReading: sensorReading })
