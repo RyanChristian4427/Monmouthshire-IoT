59348883de19f8c9d623790fb0979dfe241c24f3
Lauren Heymer <laurenheymer@gmail.com>
2019-11-25 20:51:16 +0000

    ~MR changes

--- rasp-pi/src/client.js
+++ rasp-pi/src/client.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
-const serverIp = '192.168.1.42';
-const url = `http://${serverIp}:8000/api/v1/sensorReadings/new`;
+import { API_URL } from './util/secrets';
+
+const url = `${API_URL}/sensorReadings/new`;
 
 export const postNewReading = (sensorReading) => {
-    axios.post(url, { sensorReading: sensorReading })
-        .then((res) => console.log(res))
-        .catch((err) => console.log(err));
+	axios.post(url, {sensorReading: sensorReading})
+		.then((res) => console.log(res))
+		.catch((err) => console.log(err));
 
 };
