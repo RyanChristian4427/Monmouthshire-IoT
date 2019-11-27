e1419976df85d172682c4063a46acc810a60af48
Ryan Christian <ryanchristian4427@gmail.com>
2019-11-27 23:42:52 +0000

    refactor: Removed example controller, wasn't providing value

--- express-server/src/api/v1/index.ts
+++ express-server/src/api/v1/index.ts
@@ -1,12 +1,10 @@
 import homeRoutes from './home';
-import exampleRoutes from './example';
 import sensorReadingRoutes from './sensorReadings';
 import authRoutes from './auth';
 
 const routes = [
     authRoutes,
     homeRoutes,
-    exampleRoutes,
     sensorReadingRoutes
 ];
 
