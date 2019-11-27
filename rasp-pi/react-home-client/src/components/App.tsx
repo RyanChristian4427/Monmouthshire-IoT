7196956b5ffcf3d3013142205e1c5e7940278926
Ryan Christian <ryanchristian4427@gmail.com>
2019-11-27 19:36:50 +0000

    feat: Added logic to back the loading spinner

--- rasp-pi/react-home-client/src/components/App.tsx
+++ rasp-pi/react-home-client/src/components/App.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {HeroHeader} from 'components/HeroHeader';
 import './App.scss';
+import socket from 'models/Socket';
 
 const App: React.FC = () => {
-    let isLoading = true;
+    const [isLoading, setIsLoading] = useState(false);
+
+    socket.on('initial_loading_finished', () => {
+        setIsLoading(false);
+    });
+
+    const display = (isLoading)
+        ? <h3>Loading, please wait...</h3>
+        : <h3>Please connect a sensor to begin</h3>;
 
     return (
-      <div className="home-page">
-        <HeroHeader title="Home"/>
-        <section className="card">
-          <div className={"container " + (isLoading ? "is-loading" : "")} id="layered-background">
-            <h3>Lorem Ipsum</h3>
-          </div>
-        </section>
-      </div>
+        <div className="home-page">
+            <HeroHeader title="Home"/>
+            <section className="card">
+                <div className={"container has-text-centered " + (isLoading ? "is-loading" : "")} id="layered-background">
+                    {display}
+                </div>
+            </section>
+        </div>
     );
 };
 
