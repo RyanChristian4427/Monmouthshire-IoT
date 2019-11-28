86d5a119e1dc1273f3f96f41e827222676f00421
Ryan Christian <ryanchristian4427@gmail.com>
2019-11-28 23:49:40 +0000

    feat(state-store): Added store for sensors

---
+++ rasp-pi/react-home-client/src/stores/SensorStore.ts
@@ -0,0 +1,27 @@
+import {action, observable} from 'mobx';
+import {createContext} from 'react';
+import {Sensor, SensorType} from 'models/Sensor';
+
+class SensorStore {
+    @observable
+    tempSensorList: Array<Sensor> = [
+        { id: 0, name: 'Sensor 1'},
+        { id: 1, name: 'Sensor 2'},
+        { id: 2, name: 'Sensor 3'},
+        { id: 3, name: 'Sensor 4'},
+        { id: 4, name: 'Sensor 5'},
+        { id: 5, name: 'Sensor 6'},
+        { id: 6, name: 'Sensor 7'},
+        { id: 7, name: 'Sensor 8'},
+    ];
+
+    @observable
+    indexSelectedSensor = -1;
+
+    @action
+    setSensorName(newName: string) {
+        this.tempSensorList[this.indexSelectedSensor].name = newName;
+    }
+}
+
+export const SensorStoreContext = createContext(new SensorStore());
