b398b24e1db284ed00ec9bd0bdaa333c856850ac
Ryan Christian <ryanchristian4427@gmail.com>
2019-11-28 23:53:10 +0000

    feat: Added model for a Sensor

---
+++ rasp-pi/react-home-client/src/models/Sensor.ts
@@ -0,0 +1,13 @@
+export interface Sensor {
+    id: number;
+    name: string;
+    type?: SensorType;
+}
+
+export enum SensorType {
+    kitchen,
+    bedroom,
+    bathroom,
+    livingRoom,
+    exteriorDoor
+}
