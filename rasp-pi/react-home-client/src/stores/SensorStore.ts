3cc250b36b99edbc281f966b83aeb130cce5f3d5
Ryan Christian <ryanchristian4427@gmail.com>
2019-12-02 15:41:15 +0000

    fix: Switch over hardware type to enum value

--- rasp-pi/react-home-client/src/stores/SensorStore.ts
+++ rasp-pi/react-home-client/src/stores/SensorStore.ts
@@ -7,20 +7,20 @@ import socket from 'util/sockets';
 
 class SensorStore {
     // Plz don't remove, I do need this if I want to test at all. Just swap the two
-    //@observable
-    //SensorList: Array<Sensor> = [
-    //    { nodeId: 0, name: 'Sensor 1', type: SensorType.none, hardware: HardwareType.multiSensor},
-    //    { nodeId: 1, name: 'Sensor 2', type: SensorType.none, hardware: HardwareType.smartSwitch},
-    //    { nodeId: 2, name: 'Sensor 3', type: SensorType.none, hardware: HardwareType.multiSensor},
-    //    { nodeId: 3, name: 'Sensor 4', type: SensorType.none, hardware: HardwareType.multiSensor},
-    //    { nodeId: 4, name: 'Sensor 5', type: SensorType.none, hardware: HardwareType.multiSensor},
-    //    { nodeId: 5, name: 'Sensor 6', type: SensorType.none, hardware: HardwareType.multiSensor},
-    //    { nodeId: 6, name: 'Sensor 7', type: SensorType.none, hardware: HardwareType.multiSensor},
-    //    { nodeId: 7, name: 'Sensor 8', type: SensorType.none, hardware: HardwareType.multiSensor},
-    //];
-
     @observable
-    SensorList: Array<Sensor> = [];
+    SensorList: Array<Sensor> = [
+       { nodeId: 0, name: 'Sensor 1', type: SensorType.None, hardware: HardwareType.multiSensor},
+       { nodeId: 1, name: 'Sensor 2', type: SensorType.None, hardware: HardwareType.smartSwitch},
+       { nodeId: 2, name: 'Sensor 3', type: SensorType.None, hardware: HardwareType.multiSensor},
+       { nodeId: 3, name: 'Sensor 4', type: SensorType.None, hardware: HardwareType.multiSensor},
+       { nodeId: 4, name: 'Sensor 5', type: SensorType.None, hardware: HardwareType.multiSensor},
+       { nodeId: 5, name: 'Sensor 6', type: SensorType.None, hardware: HardwareType.multiSensor},
+       { nodeId: 6, name: 'Sensor 7', type: SensorType.None, hardware: HardwareType.multiSensor},
+       { nodeId: 7, name: 'Sensor 8', type: SensorType.None, hardware: HardwareType.multiSensor},
+    ];
+
+    // @observable
+    // SensorList: Array<Sensor> = [];
 
 
     @observable
@@ -32,7 +32,7 @@ class SensorStore {
     }
 
     @action
-    setSensorType(newType: number): void {
+    setSensorType(newType: SensorType): void {
         this.SensorList[this.indexSelectedSensor].type = newType;
         socket.emit('sensor_update', {nodeId: this.indexSelectedSensor, type: newType, name: this.SensorList[this.indexSelectedSensor].name});
     }
