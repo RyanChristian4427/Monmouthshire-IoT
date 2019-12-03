2a421f324e9b9007a684692ce4f9cee731a13e19
Ryan Christian <ryanchristian4427@gmail.com>
2019-12-03 15:32:34 +0000

    feat: More or less finalized final Neo4j node structure

--- react-client/src/models/Neo4J.ts
+++ react-client/src/models/Neo4J.ts
@@ -3,7 +3,6 @@ import {MeasureType, RoomType} from 'models/Sensor';
 export interface Node {
     userId: number;
     value: number | string;
-    unit: string;
     measures: MeasureType;
     roomType: RoomType;
     timestamp: string;
