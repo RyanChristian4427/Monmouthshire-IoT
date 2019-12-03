132e5ea9835f97dca157540157530c25aee27ea5
Ryan Christian <ryanchristian4427@gmail.com>
2019-12-03 00:52:58 +0000

    chore(formatting): Fixed formatting in sensor reading controller

--- express-server/src/database/models/sensorReading.ts
+++ express-server/src/database/models/sensorReading.ts
@@ -1,8 +1,10 @@
+import Integer from 'neo4j-driver/types/integer';
+
 export default interface SensorReading {
     id: string;
     nodeId: number;
     userId: number;
     type: string;
     unit: string;
-    value: number;
+    value: Integer;
 }
