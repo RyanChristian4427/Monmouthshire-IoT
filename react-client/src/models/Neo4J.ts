2fb9e21b84bb4f7e0a62a5d61fb70fef8ac0d11d
Ryan Christian <ryanchristian4427@gmail.com>
2019-12-03 00:55:36 +0000

    feat: Types for Nodes and other Neo4j stuff

---
+++ react-client/src/models/Neo4J.ts
@@ -0,0 +1,10 @@
+import {MeasureType, RoomType} from 'models/Sensor';
+
+export interface Node {
+    userId: number,
+    value: number | string,
+    unit: string,
+    measures: MeasureType,
+    roomType: RoomType,
+    timestamp: any
+}
---
+++ react-client/src/models/Sensor.ts
@@ -0,0 +1,16 @@
+export enum RoomType {
+    kitchen,
+    bedroom,
+    bathroom,
+    livingRoom,
+    exteriorDoor,
+    none
+}
+
+export enum MeasureType {
+    humidity,
+    temperature,
+    luminance,
+    movement,
+    electricFlow
+}
