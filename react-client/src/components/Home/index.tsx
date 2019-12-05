6fe13d867b5ebf2b339d9b634ee1801dede904db
Ryan Christian <ryanchristian4427@gmail.com>
2019-12-05 00:35:56 +0000

    feat: Adding Radial Chart to home page

--- react-client/src/components/Home/index.tsx
+++ react-client/src/components/Home/index.tsx
@@ -4,6 +4,7 @@ import {LineGraph} from 'components/graphs/LineGraph';
 import {HeroHeader} from 'components/HeroHeader';
 
 import './Home.scss';
+import {PieChart} from 'components/graphs/PieChart';
 
 
 export const Home: React.FC = () => {
@@ -17,8 +18,7 @@ export const Home: React.FC = () => {
                                    yAxisTitle="Temperature" height={600} width={600}/>
                     </section>
                     <section className="section chart-card">
-                        <LineGraph title="Temperature Over Time Per Room" xAxisTitle="Time"
-                                   yAxisTitle="Temperature" height={600} width={600}/>
+                        <PieChart title="Percent of Time Spent in Room" height={600} width={600}/>
                     </section>
                 </div>
             </section>
