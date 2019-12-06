2fd39d776f9ae4817cc1264a35df52ef05676e39
Ryan Christian <ryanchristian4427@gmail.com>
2019-12-06 21:53:17 +0000

    feat: Updated SensorStore

--- react-client/src/stores/SensorDataStore.ts
+++ react-client/src/stores/SensorDataStore.ts
@@ -1,14 +1,39 @@
-import {observable} from 'mobx';
+import {action, observable} from 'mobx';
 import {createContext} from 'react';
 
-import {Node} from 'models/Neo4J';
+import {ProcessedNodeData} from 'models/Neo4J';
 
-class SensorDataStore {
-    @observable kitchenDataList: Node[] = [];
-    @observable bedroomDataList: Node[] = [];
-    @observable bathroomDataList: Node[] = [];
-    @observable livingRoomDataList: Node[] = [];
-    @observable exteriorDoorDataList: Node[] = [];
+export class SensorDataStore {
+    @observable kitchenDataList: ProcessedNodeData[] = [];
+    @observable bedroomDataList: ProcessedNodeData[] = [];
+    @observable bathroomDataList: ProcessedNodeData[] = [];
+    @observable livingRoomDataList: ProcessedNodeData[] = [];
+    @observable exteriorDoorDataList: ProcessedNodeData[] = [];
+
+    @action
+    setKitchenData(dataList: ProcessedNodeData[]): void {
+        this.kitchenDataList = dataList;
+    }
+
+    @action
+    setBedroomData(dataList: ProcessedNodeData[]): void {
+        this.bedroomDataList = dataList;
+    }
+
+    @action
+    setBathroomData(dataList: ProcessedNodeData[]): void {
+        this.bathroomDataList = dataList;
+    }
+
+    @action
+    setLivingRoomData(dataList: ProcessedNodeData[]): void {
+        this.livingRoomDataList = dataList;
+    }
+
+    @action
+    setExteriorDoorData(dataList: ProcessedNodeData[]): void {
+        this.exteriorDoorDataList = dataList;
+    }
 }
 
 export const SensorDataStoreContext = createContext(new SensorDataStore());
