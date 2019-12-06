2df313a66cd898524fcf019e91bee3cc36feb0a3
Ryan Christian <ryanchristian4427@gmail.com>
2019-12-06 21:48:54 +0000

    feat: Updated requests to return correct data type

--- react-client/src/services/requests.ts
+++ react-client/src/services/requests.ts
@@ -1,13 +1,13 @@
 import {apiService} from 'ts-api-toolkit';
+import {RoomData} from 'models/Neo4J';
 
-export const getTemperatures = (currentUser: string): Promise<string> => {
+export const getTemperatures = (currentUser: string): Promise<RoomData[]> => {
     return new Promise((resolve, reject) => {
         apiService.get('/sensorReadings/temperature', encodeURIComponent(currentUser))
             .then(({ data }) => {
                 resolve(data);
             })
             .catch(({ response }) => {
-                console.log(response);
                 if (response !== undefined) {
                     reject(response.data.message);
                 } else {
