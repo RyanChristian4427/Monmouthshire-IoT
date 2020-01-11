e34b6aa9dc8880c4e727bf614a822bff6fdb5133
Ryan Christian <ryanchristian4427@gmail.com>
2020-01-11 15:13:14 +0000

    feat: Added LoginUser type definition

--- packages/apps/web-public/src/app.tsx
+++ packages/apps/web-public/src/app.tsx
@@ -2,12 +2,10 @@ import preact, { h } from 'preact';
 import { Route, Router, RouterOnChangeArgs } from 'preact-router';
 
 import Home from 'routes/home';
+import Login from 'routes/login';
 import Profile from 'routes/profile';
-import Header from 'components/header';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 if ((module as any).hot) {
-    // tslint:disable-next-line:no-var-requires
     require('preact/debug');
 }
 
@@ -22,12 +20,12 @@ export const App: preact.FunctionalComponent = () => {
     console.log(currentUrl);
 
     return (
-        <div id="app">
-            <Header />
+        <div id='app'>
             <Router onChange={handleRoute}>
-                <Route path="/" component={Home} />
-                <Route path="/profile/" component={Profile} user="me" />
-                <Route path="/profile/:user" component={Profile} />
+                <Route path='/' component={Home} />
+                <Route path='/login' component={Login} />
+                <Route path='/profile/' component={Profile} user='me' />
+                <Route path='/profile/:user' component={Profile} />
             </Router>
         </div>
     );
