444c2c1a1967c3c7abe259ab394c9e2a01b5b005
Ryan Christian <ryanchristian4427@gmail.com>
2020-01-15 00:17:39 +0000

    feat(component): Wrapped login in form to get browser autocomplete

--- packages/apps/web-public/src/routes/login/index.tsx
+++ packages/apps/web-public/src/routes/login/index.tsx
@@ -2,7 +2,6 @@ import preact, { h } from 'preact';
 import { useState } from 'preact/hooks';
 import { route } from 'preact-router';
 
-import Header from 'components/header';
 import * as style from './style.scss';
 
 import { login } from 'services/api';
@@ -19,51 +18,53 @@ const Login: preact.FunctionalComponent = () => {
         login(credentials).then((result) => {
             setInProgress(false);
             if (result) setErrors(result);
-            else route('/home');
+            else route('/');
         });
     };
 
     return (
         <div class={style.loginPage}>
-            <Header title="Login" withSettingsMenu={false} />
             <section class={style.card}>
                 <div class="container" id={style.layeredBackground}>
-                    <div class="field">
-                        <label class="label">User</label>
-                        <div class="control">
-                            <input
-                                class="input"
-                                type="text"
-                                placeholder="Username"
-                                value={email}
-                                onChange={(e): void => setEmail((e.target as HTMLInputElement).value)}
-                            />
+                    <form>
+                        <div class="field">
+                            <label class="label">Email</label>
+                            <div class="control">
+                                <input
+                                    class="input"
+                                    type="text"
+                                    placeholder="Email"
+                                    value={email}
+                                    onChange={(e): void => setEmail((e.target as HTMLInputElement).value)}
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div class="field">
-                        <label class="label">Password</label>
-                        <div class="control">
-                            <input
-                                class="input"
-                                type="password"
-                                placeholder="Password"
-                                value={password}
-                                onChange={(e): void => setPassword((e.target as HTMLInputElement).value)}
-                            />
+                        <div class="field">
+                            <label class="label">Password</label>
+                            <div class="control">
+                                <input
+                                    class="input"
+                                    type="password"
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={(e): void => setPassword((e.target as HTMLInputElement).value)}
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div class="level">
-                        <div class="level-left" />
-                        <div class="level-right">
-                            <button
-                                class={'button is-whitesmoke-light level-item ' + (inProgress ? 'is-loading' : '')}
-                                onClick={(): void => submitDetails()}
-                            >
-                                Submit
-                            </button>
+                        <div class="level">
+                            <div class="level-left" />
+                            <div class="level-right">
+                                <button
+                                    class={'button is-whitesmoke-light level-item ' + (inProgress ? 'is-loading' : '')}
+                                    type="button"
+                                    onClick={(): void => submitDetails()}
+                                >
+                                    Submit
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    <h2 class={style.error + ' is-size-5' + (errors ? '' : 'is-hidden')}>{errors}</h2>
+                        <h2 class={style.error + ' is-size-5' + (errors ? '' : 'is-hidden')}>{errors}</h2>
+                    </form>
                 </div>
             </section>
         </div>
