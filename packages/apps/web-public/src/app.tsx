35995b28428c39f8a29caa3155149c5cd07116d6
Ryan Christian <ryanchristian4427@gmail.com>
2020-01-15 00:18:22 +0000

    feat: Revamped routing for better component control

--- packages/apps/web-public/src/app.tsx
+++ packages/apps/web-public/src/app.tsx
@@ -1,27 +1,47 @@
 import preact, { h } from 'preact';
-import { Route, Router, RouterOnChangeArgs } from 'preact-router';
+import { useState } from 'preact/hooks';
+import { getCurrentUrl, Route, Router, RouterOnChangeArgs } from 'preact-router';
 
+import Header from 'components/header';
 import Home from 'routes/home';
 import Login from 'routes/login';
 import Profile from 'routes/profile';
 
-// if ((module as any).hot) {
-//     require('preact/debug');
-// }
+interface RouteInfo {
+    title: string;
+    subtitle?: string;
+    settings: boolean;
+}
+
+interface Routes {
+    [key: string]: RouteInfo;
+}
 
 export const App: preact.FunctionalComponent = () => {
-    let currentUrl = '';
-    const handleRoute = (e: RouterOnChangeArgs): void => {
-        currentUrl = e.url;
+    const [currentUrl, setCurrentUrl] = useState<string>(getCurrentUrl());
+
+    const routes: Routes = {
+        '/login': {
+            title: 'Login',
+            subtitle: '',
+            settings: false,
+        },
+        '/': {
+            title: 'Home',
+            subtitle: 'Sweet Home',
+            settings: true,
+        },
     };
 
-    // This is just here to stop the linter from yelling about this variable
-    // being unused
-    console.log(currentUrl);
+    const renderHeader = (): preact.ComponentChild => {
+        const routeInfo = routes[currentUrl];
+        return <Header title={routeInfo.title} subtitle={routeInfo.subtitle} withSettingsMenu={routeInfo.settings} />;
+    };
 
     return (
         <div id="app">
-            <Router onChange={handleRoute}>
+            {renderHeader()}
+            <Router onChange={(e: RouterOnChangeArgs): void => setCurrentUrl(e.url)}>
                 <Route path="/" component={Home} />
                 <Route path="/login" component={Login} />
                 <Route path="/profile/" component={Profile} user="me" />
