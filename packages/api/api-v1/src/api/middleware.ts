2c945a7e526acecde5f3e590f1143b43f35dd7e4
Ryan Christian <ryanchristian4427@gmail.com>
2020-01-23 19:37:18 +0000

    feat: Upgraded auth middleware with much better error handling

--- packages/api/api-v1/src/api/middleware.ts
+++ packages/api/api-v1/src/api/middleware.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+
+import logger from 'src/util/logger';
 import { JWT_SECRET } from 'src/util/secrets';
 
 export const checkToken = (req: Request, res: Response, next: NextFunction): Response | NextFunction => {
@@ -10,16 +12,25 @@ export const checkToken = (req: Request, res: Response, next: NextFunction): Res
 
     if (token) {
         jwt.verify(token, JWT_SECRET, (err) => {
+            const decodedToken: any = jwt.decode(token);
             if (err) {
+                logger.debug(`JWT verification for user ${decodedToken.email} has failed due to ${err.message}`);
+                if (err.message == 'jwt expired') {
+                    return res.status(401).json({
+                        success: false,
+                        message: 'Access token has expired',
+                    });
+                }
                 return res.status(401).json({
                     success: false,
-                    message: 'Auth token is not valid',
+                    message: 'Access token is not valid',
                 });
-            } else {
-                next();
             }
+            logger.debug(`JWT verification for user ${decodedToken.email} has succeeded`);
+            next();
         });
     } else {
+        logger.debug('JWT verification for an unknown user has failed as they did not supply a token');
         return res.status(401).json({
             success: false,
             message: 'Auth token is not supplied',
