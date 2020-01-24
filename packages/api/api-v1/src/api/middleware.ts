d9d25ff822804d00fddf478feda7f04a023654ef
Ryan Christian <ryanchristian4427@gmail.com>
2020-01-24 23:00:05 +0000

    chore: Made middleware logic easier to follow

--- packages/api/api-v1/src/api/middleware.ts
+++ packages/api/api-v1/src/api/middleware.ts
@@ -13,8 +13,14 @@ export const checkToken = (req: Request, res: Response, next: NextFunction): Res
     if (token) {
         jwt.verify(token, JWT_SECRET, (err) => {
             const decodedToken: any = jwt.decode(token);
-            if (err) {
-                logger.debug(`JWT verification for user ${decodedToken.email} has failed due to ${err.message}`);
+            const user = decodedToken ? decodedToken.email : 'Unknown';
+
+            if (!err) {
+                logger.debug(`JWT verification for user ${user} has succeeded`);
+                next();
+            } else {
+                logger.debug(`JWT verification for user ${user} has failed due to ${err.message}`);
+
                 if (err.message == 'jwt expired') {
                     return res.status(401).json({
                         success: false,
@@ -25,20 +31,19 @@ export const checkToken = (req: Request, res: Response, next: NextFunction): Res
                         success: false,
                         message: 'Access token has invalid signature',
                     });
+                } else {
+                    return res.status(401).json({
+                        success: false,
+                        message: 'Access token is not valid',
+                    });
                 }
-                return res.status(401).json({
-                    success: false,
-                    message: 'Access token is not valid',
-                });
             }
-            logger.debug(`JWT verification for user ${decodedToken.email} has succeeded`);
-            next();
         });
     } else {
         logger.debug('JWT verification for an unknown user has failed as they did not supply a token');
         return res.status(401).json({
             success: false,
-            message: 'Auth token is not supplied',
+            message: 'Access token is not supplied',
         });
     }
 };
