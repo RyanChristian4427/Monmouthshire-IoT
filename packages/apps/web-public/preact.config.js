cc895d01f9ee054cefd12582a2376efb8db61134
Ryan Christian <ryanchristian4427@gmail.com>
2020-01-25 20:42:52 +0000

    chore: Switched public Preact config to nicer format

--- packages/apps/web-public/preact.config.js
+++ packages/apps/web-public/preact.config.js
@@ -1,23 +1,27 @@
-import { resolve } from 'path';
+export default {
+    /**
+     * Function that mutates the original webpack config.
+     * Supports asynchronous changes when a promise is returned (or it's an async function).
+     *
+     * @param {object} config - original webpack config.
+     * @param {object} env - options passed to the CLI.
+     * @param {WebpackConfigHelpers} helpers - object with useful helpers for working with the webpack config.
+     * @param {object} options - this is mainly relevant for plugins (will always be empty in the config), default to an empty object
+     **/
+    webpack(config, env, helpers, options) {
+        const css = helpers.getLoadersByName(config, 'css-loader')[0];
+        css.loader.options.modules = false;
 
-export default function(config, env, helpers) {
-    const css = helpers.getLoadersByName(config, 'css-loader')[0];
-    css.loader.options.modules = false;
+        // Sets default import to 'src/'
+        config.resolve.modules.push(env.src);
 
-    // Use any `index` file, not just index.js
-    config.resolve.alias['preact-cli-entrypoint'] = resolve(process.cwd(), 'src', 'index');
-
-    config.resolve.modules.push(env.src);
-
-    config.devServer.proxy = [
-        {
-            // proxy requests matching a pattern:
-            path: '/api/v1',
-
-            // where to proxy to:
-            target: 'http://localhost:8000',
-        },
-    ];
-
-    return config;
-}
+        if (!env.isProd) {
+            config.devServer.proxy = [
+                {
+                    path: '/api/v1',
+                    target: 'http://localhost:8000',
+                },
+            ];
+        }
+    },
+};
