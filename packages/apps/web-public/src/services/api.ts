4c74f3f1a2b248647e02d294475c8cfcf36fad97
Ryan Christian <ryanchristian4427@gmail.com>
2020-01-25 20:57:25 +0000

    refactor: Refactored API requests in web-public

--- packages/apps/web-public/src/services/api.ts
+++ packages/apps/web-public/src/services/api.ts
@@ -1,42 +1,38 @@
 import { apiService, jwtService } from 'ts-api-toolkit';
-import { LoginUser, RegistrationUser } from 'models/User';
+import { LoginUser, RegistrationUser, User } from 'models/User';
 
-export const login = (credentials: LoginUser): Promise<string> => {
-    return new Promise((resolve) => {
-        apiService
-            .post('users/login', credentials)
-            .then(({ data }) => {
-                jwtService.saveToken(data.user.token);
-                resolve();
-            })
-            .catch(({ response }) => {
-                if (response.data !== '') {
-                    resolve(response.data.message);
-                } else {
-                    resolve('Unknown error while logging in');
-                }
-            });
-    });
+export const login = async (credentials: LoginUser): Promise<User | string> => {
+    return await apiService
+        .post('users/login', credentials)
+        .then(({ data }) => {
+            jwtService.saveToken(data.user.token);
+            return data.user;
+        })
+        .catch(({ response }) => {
+            if (response.data !== '') {
+                return response.data.message;
+            } else {
+                return 'Unknown error while logging in';
+            }
+        });
 };
 
-export const logout = (): void => {
-    jwtService.destroyToken();
+export const logout = async (): Promise<void> => {
+    await jwtService.destroyToken();
 };
 
-export const register = (credentials: RegistrationUser): Promise<string> => {
-    return new Promise((resolve) => {
-        apiService
-            .post('users/register', credentials)
-            .then(({ data }) => {
-                jwtService.saveToken(data.user.token);
-                resolve();
-            })
-            .catch(({ response }) => {
-                if (response.data !== '') {
-                    resolve(response.data.message);
-                } else {
-                    resolve('Unknown error while registering');
-                }
-            });
-    });
+export const register = async (credentials: RegistrationUser): Promise<User | string> => {
+    return await apiService
+        .post('users/register', credentials)
+        .then(({ data }) => {
+            jwtService.saveToken(data.user.token);
+            return data.user;
+        })
+        .catch(({ response }) => {
+            if (response.data !== '') {
+                return response.data.message;
+            } else {
+                return 'Unknown error while registering';
+            }
+        });
 };
