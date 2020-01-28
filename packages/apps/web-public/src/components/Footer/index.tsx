101ba7b264a33e573f889b49d2c97bc63d14e3b4
Ryan Christian <ryanchristian4427@gmail.com>
2020-01-28 17:03:47 +0000

    fix: Fixed acknowledgements in the footer

--- packages/apps/web-public/src/components/Footer/index.tsx
+++ packages/apps/web-public/src/components/Footer/index.tsx
@@ -6,7 +6,8 @@ const Footer: preact.FunctionalComponent = () => (
             <span>
                 <a href="https://github.com/RyanChristian4427">GitHub </a>
             </span>
-            <span>&copy; 2019 Ryan Christian</span>
+            <span>&copy; 2019 Ryan &quot;The God&quot; Christian</span>
+            <span style={{ fontSize: '.65em' }}> and Lauren Heymer</span>
         </div>
     </div>
 );
