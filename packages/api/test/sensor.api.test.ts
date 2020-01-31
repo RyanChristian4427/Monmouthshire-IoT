28997ed489e1ce73c0fbecb310353bd29b2221bf
Ryan Christian <ryanchristian4427@gmail.com>
2020-01-31 22:01:59 +0000

    test(api): Added sensor data test in api

---
+++ packages/api/test/sensor.api.test.ts
@@ -0,0 +1,38 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+import { app } from 'src/app';
+import { login } from 'test/common';
+
+chai.use(chaiHttp);
+
+describe('POST /api/v1/sensors/all', () => {
+    it('should return 200 OK with DAta', async (done) => {
+        chai.request(app)
+            .get('/api/v1/sensors/all')
+            .set('Authorization', `Bearer ${await login()}`)
+            .end((err, res) => {
+                expect(res.status).toBe(200);
+                expect(res.body.success).toBe(true);
+                expect(res.body.message).toBe('Data retrieval successful');
+                done();
+            });
+    });
+
+    // it('should return 401 Bad Request', async (done) => {
+    //     chai.request(app)
+    //         .get('/api/v1/sensors/all')
+    //         // TODO Fix this once you can request data for an email, and not rely on tokens
+    //         .set(
+    //             'Authorization',
+    //             'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJmYWtlc21va2V0ZXN0QGV4YW1wbG' +
+    //                 'UuY29tIiwiaWF0IjoxNTgwNTA3NjQzLCJleHAiOjE4ODA1OTQwNDN9.47P9F5C_YcxMVBxcwBa3_HAV1dDacm-va97sNCL5iI8',
+    //         )
+    //         .end((err, res) => {
+    //             console.log(res);
+    //             expect(res.status).toBe(401);
+    //             expect(res.body.success).toBe(false);
+    //             done();
+    //         });
+    // });
+});
