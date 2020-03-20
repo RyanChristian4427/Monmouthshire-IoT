c052fe550386bc3ea2a508812de507093803b999
Ryan Christian <ryanchristian4427@gmail.com>
2020-03-20 15:49:58 +0000

    fix: Fixing authGuard with help from @renHeymer

--- packages/apps/web-public/src/routes/App.tsx
+++ packages/apps/web-public/src/routes/App.tsx
@@ -1,36 +1,24 @@
-import preact, { h } from 'preact';
-import { useContext, useState } from 'preact/hooks';
-import { getCurrentUrl, route, Route, Router, RouterOnChangeArgs } from 'preact-router';
+import { FunctionalComponent, h } from 'preact';
+import { useContext } from 'preact/hooks';
+import { route, Route, Router, RouterOnChangeArgs } from 'preact-router';
 
 import Auth from 'routes/auth';
 import Home from 'routes/home';
 import { AuthStoreContext } from 'stores';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-if ((module as any).hot) {
-    // tslint:disable-next-line:no-var-requires
-    require('preact/debug');
-}
-
-const App: preact.FunctionalComponent = () => {
+const App: FunctionalComponent = () => {
     const authStore = useContext(AuthStoreContext);
-
-    const [currentUrl, setCurrentUrl] = useState<string>(getCurrentUrl());
 
     const publicRoutes = ['/register', '/login'];
 
-    const authGuard = (): void => {
-        if (!publicRoutes.includes(currentUrl) && !authStore.isAuthenticated) {
-            // TODO: Necessary until https://github.com/preactjs/preact-router/issues/357 is resolved
-            route('/login');
-        }
+    const authGuard = (e: RouterOnChangeArgs): void => {
+        if (!publicRoutes.includes(e.url) && !authStore.isAuthenticated) route('/login');
     };
 
     return (
         <div id="app">
-            {authGuard()}
-            <Router onChange={(e: RouterOnChangeArgs): void => setCurrentUrl(e.url)}>
-                <Home path="/" />
+            <Router onChange={authGuard}>
+                <Route path="/" component={Home} />
                 <Route path="/login" component={Auth} />
                 <Route path="/register" component={Auth} />
             </Router>
