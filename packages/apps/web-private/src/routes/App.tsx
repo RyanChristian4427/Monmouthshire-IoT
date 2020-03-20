ccd3a9e28fb2b2565ff7986eab74b8959fa7b030
Ryan Christian <ryanchristian4427@gmail.com>
2020-03-20 20:14:03 +0000

    fix: Web-private was still using jwtService

--- packages/apps/web-private/src/routes/App.tsx
+++ packages/apps/web-private/src/routes/App.tsx
@@ -1,34 +1,21 @@
-import preact, { h } from 'preact';
-import { useState } from 'preact/hooks';
-import Router, { getCurrentUrl, Route, route, RouterOnChangeArgs } from 'preact-router';
-import { jwtService } from 'ts-api-toolkit';
+import { FunctionalComponent, h } from 'preact';
+import Router, { Route, route, RouterOnChangeArgs } from 'preact-router';
+import { authStorageService } from 'ts-api-toolkit';
 
 import Home from 'routes/home';
 import Login from 'routes/login';
 import Register from 'routes/register';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-if ((module as any).hot) {
-    // tslint:disable-next-line:no-var-requires
-    require('preact/debug');
-}
-
-const App: preact.FunctionalComponent = () => {
-    const [currentUrl, setCurrentUrl] = useState<string>(getCurrentUrl());
-
+const App: FunctionalComponent = () => {
     const publicRoutes = ['/register', '/login'];
 
-    const authGuard = (): void => {
-        if (!publicRoutes.includes(currentUrl) && jwtService.getToken() == null) {
-            // TODO: Necessary until https://github.com/preactjs/preact-router/issues/357 is resolved
-            route('/login');
-        }
+    const authGuard = (e: RouterOnChangeArgs): void => {
+        if (!publicRoutes.includes(e.url) && !authStorageService.getToken()) route('/auth/login');
     };
 
     return (
         <div id="app">
-            {authGuard()}
-            <Router onChange={(e: RouterOnChangeArgs): void => setCurrentUrl(e.url)}>
+            <Router onChange={authGuard}>
                 <Route path="/" component={Home} />
                 <Route path="/login" component={Login} />
                 <Route path="/register" component={Register} />
