47462700951fef312e55d78d4250acadf089ed53
Ryan Christian <ryanchristian4427@gmail.com>
2020-03-22 19:18:56 +0000

    feat: Adding basic chart using live data

--- packages/apps/web-public/src/routes/home/index.tsx
+++ packages/apps/web-public/src/routes/home/index.tsx
@@ -1,11 +1,34 @@
 import { FunctionalComponent, h } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { DateTime } from 'luxon';
+import { RoomType, SensorDataResponse } from '@core/types';
 
 import Footer from 'components/Footer';
 import Navbar from 'components/Navbar';
+import { getAllSensors } from 'services/api/sensors';
 
 import './style.scss';
 
 const Home: FunctionalComponent = () => {
+    const [bedroomTemp, setBedroomTemp] = useState<SensorDataResponse[]>([]);
+
+    useEffect(() => {
+        getAllSensors().then((response) => {
+            if (typeof response != 'string') {
+                response.map((roomResponse) => {
+                    if (roomResponse.roomType === RoomType.bedroom) {
+                        roomResponse.temperature.map((sensorDataResponse) => {
+                            setBedroomTemp((oldData) => [...oldData, sensorDataResponse]);
+                        });
+                    }
+                });
+            }
+        });
+    }, []);
+
+    console.log(bedroomTemp);
+
     return (
         <div class="home-page">
             <section class="hero is-fullheight is-default is-bold">
@@ -16,9 +39,24 @@ const Home: FunctionalComponent = () => {
                     <div class="container has-text-centered">
                         <div class="columns is-vcentered">
                             <div class="column is-5">
-                                <figure class="image">
-                                    <img src="https://picsum.photos/800/600/?random" alt="Description" />
-                                </figure>
+                                <ResponsiveContainer width="99%" height={300}>
+                                    <LineChart data={bedroomTemp} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                                        <Line type="monotone" dataKey="value" stroke="#8884d8" />
+                                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                                        <XAxis
+                                            dataKey="time"
+                                            tickFormatter={(timestamp): string =>
+                                                DateTime.fromISO(timestamp).toLocaleString(DateTime.DATETIME_MED)
+                                            }
+                                        />
+                                        <YAxis />
+                                        <Tooltip
+                                            labelFormatter={(timestamp: string): string =>
+                                                DateTime.fromISO(timestamp).toLocaleString(DateTime.DATETIME_MED)
+                                            }
+                                        />
+                                    </LineChart>
+                                </ResponsiveContainer>
                             </div>
                             <div class="column is-6 is-offset-1">
                                 <h1 class="title is-2">Superhero Scaffolding</h1>
