c359df9210954036291909d3522013b99223926a
Ryan Christian <ryanchristian4427@gmail.com>
2020-03-25 02:46:47 +0000

    chore(linting): Cleaning import order in LineChart

--- packages/apps/web-public/src/components/LineChart/index.tsx
+++ packages/apps/web-public/src/components/LineChart/index.tsx
@@ -1,8 +1,8 @@
 import { ComponentChild, FunctionalComponent, h } from 'preact';
-
-import { RechartsSensorDataResponse } from 'models/Recharts';
 import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import { DateTime } from 'luxon';
+
+import { RechartsSensorDataResponse } from 'models/Recharts';
 
 const colors = ['#003f5c', '#2f4b7c', '#665191', '#a05195', '#d45087', '#f95d6a', '#ff7c43', '#ffa600'];
 
