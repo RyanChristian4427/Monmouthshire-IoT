9c38d79bbd02f55f8798c05670056911b92fde4f
Ryan Christian <ryanchristian4427@gmail.com>
2020-04-06 00:54:44 +0100

    feat: Added container component for the graphs

---
+++ packages/apps/web-public/src/components/GraphContainer/index.tsx
@@ -0,0 +1,51 @@
+import { ComponentChild, FunctionalComponent, h } from 'preact';
+import { useState } from 'preact/hooks';
+
+import './style.scss';
+
+interface IProps {
+    childGraph: ComponentChild;
+}
+
+export const GraphContainer: FunctionalComponent<IProps> = (props: IProps) => {
+    const [startDate, setStartDate] = useState(() => {
+        const today = new Date();
+        today.setDate(today.getDate() - 7);
+        return today;
+    });
+    const [dueDate, setDueDate] = useState(new Date());
+
+    const zeroPad = (value: number): string => (value < 10 ? '0' : '') + value;
+
+    return (
+        <div class="container graph-container is-flex">
+            {props.childGraph}
+            <div class="is-pulled-right">
+                <div class="field">
+                    <label class="label">Start Date</label>
+                    <div class="control">
+                        <input
+                            type="date"
+                            value={`${startDate.getFullYear().toString()}-${zeroPad(
+                                startDate.getMonth() + 1,
+                            )}-${zeroPad(startDate.getDate())}`}
+                            onInput={(e): void => setStartDate(new Date((e.target as HTMLInputElement).value))}
+                        />
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">End Date</label>
+                    <div class="control">
+                        <input
+                            type="date"
+                            value={`${dueDate.getFullYear().toString()}-${zeroPad(dueDate.getMonth() + 1)}-${zeroPad(
+                                dueDate.getDate(),
+                            )}`}
+                            onInput={(e): void => setDueDate(new Date((e.target as HTMLInputElement).value))}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
